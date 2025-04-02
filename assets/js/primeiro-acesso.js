29f1f4b291f21263e28fb508b1b7c236635314bf
marcos-md <104793659+marcos-md@users.noreply.github.com>
2025-04-01 23:36:20 -0300

    Correção do campo mascara

--- assets/js/primeiro-acesso.js
+++ assets/js/primeiro-acesso.js
@@ -1,13 +1,9 @@
 $( document ).ready(function() {
 
     /*Máscaras de entrada====================================================================*/
-    //$('#cpf').inputmask('999.999.999-99'); // Aplica a máscara de CPF
-    $('#dataNascimento').inputmask('99/99/9999'); // Aplica a máscara de data de nascimento
-    $('#celular').inputmask('(99) 99999-9999'); // Aplica a máscara de celular
-    
-    $('#cpf').inputmask('999.999.999-99', {
-        autoUnmask: true
-    });
+    $('#cpf').inputmask('999.999.999-99', { autoUnmask: true }); // Aplica a máscara de CPF
+    $('#dataNascimento').inputmask('99/99/9999', { autoUnmask: true }); // Aplica a máscara de data de nascimento
+    $('#celular').inputmask('(99) 99999-9999', { autoUnmask: true }); 
 
     /*Input text Caixa Alta====================================================================*/
     document.querySelectorAll('input[type="text"]').forEach(function(input) {
