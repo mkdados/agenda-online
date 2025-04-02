b810632093171885c947eea21c0aec285732a584
marcos-md <104793659+marcos-md@users.noreply.github.com>
2025-04-01 23:16:26 -0300

    Ajuste mascara campo input

--- assets/js/primeiro-acesso.js
+++ assets/js/primeiro-acesso.js
@@ -1,9 +1,13 @@
 $( document ).ready(function() {
 
     /*Máscaras de entrada====================================================================*/
-    $('#cpf').inputmask('999.999.999-99'); // Aplica a máscara de CPF
+    //$('#cpf').inputmask('999.999.999-99'); // Aplica a máscara de CPF
     $('#dataNascimento').inputmask('99/99/9999'); // Aplica a máscara de data de nascimento
     $('#celular').inputmask('(99) 99999-9999'); // Aplica a máscara de celular
+    
+    $('#cpf').inputmask('999.999.999-99', {
+        autoUnmask: true
+    });
 
     /*Input text Caixa Alta====================================================================*/
     document.querySelectorAll('input[type="text"]').forEach(function(input) {
