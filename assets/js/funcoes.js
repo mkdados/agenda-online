11ed58b11bb1d99b45ac0744b9da924e8314d99b
marcos-md <104793659+marcos-md@users.noreply.github.com>
2025-04-22 22:42:04 -0300

    Primeiro acesso

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,16 +1,62 @@
-$( document ).ready(function() {
-
-
-     /*Carrega carousel login====================================================================*/
-     // fetch('models/carousel-login.html')
-     // .then(response => response.text())
-     // .then(data => {
-     // document.getElementById('carousel-login').outerHTML = data;
-     // });
-
-     
+// Função para fazer o login usando uma API
+  document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
     
-});
+    // Exibe o loader
+    document.getElementById('loader').style.display = 'flex';
 
+    // Obtendo os valores dos campos
+    const emailCpf = document.getElementById('emailCpf').value;
+    const password = document.getElementById('password').value;
+    
+    // Validação simples de entrada
+    if (!emailCpf || !password) {
+      // Usando SweetAlert2 para exibir uma mensagem de erro
+      Swal.fire({
+        icon: 'error',
+        title: 'Campos inválidos',
+        text: 'Por favor, preencha todos os campos.',
+      });
+      document.getElementById('loader').style.display = 'none'; // Esconde o loader se os campos não estiverem preenchidos
+      return;
+    }
 
-   
+    // Fazendo a requisição para a API
+    fetch('http://localhost:3000/users/1', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Esconde o loader após a resposta
+      document.getElementById('loader').style.display = 'none';
+
+      if (data.email == emailCpf) {
+        // Exibe um alerta de sucesso usando SweetAlert2
+        Swal.fire({
+          icon: 'success',
+          title: 'Login bem-sucedido',
+          text: 'Você foi logado com sucesso.',
+        });
+      } else {
+        // Exibe um alerta de erro usando SweetAlert2
+        Swal.fire({
+          icon: 'error',
+          title: 'Credenciais inválidas',
+          text: 'Tente novamente.',
+        });
+      }
+    })
+    .catch(error => {
+      // Esconde o loader após erro
+      document.getElementById('loader').style.display = 'none';
+      // Exibe um alerta de erro usando SweetAlert2
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro',
+        text: 'Ocorreu um erro. Tente novamente.',
+      });
+    });
+  });
